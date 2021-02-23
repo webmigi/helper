df7b7d0ab2b84324a527cf4a2d733f65a629d483
migi <wenmigi.yt@gmail.com>
2021-02-24 02:08:45 +0300

    testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import "./scss/index.scss"
+import "@/scss/index.scss"
+import {Input} from '@/components/input/Input'
+import {Components} from '@/core/Components'
+
+const element = new Components('#app', {
+  components: [Input]
+})
+
+element.render()
